18081b76602bd947f096beac0595402fbff1f029
Matt Watson <Matt.Watson@orasi.com>
2014-08-26 20:56:39 -0400

    updated architecture

--- selenium_test/tests/Login_Test.py
+++ selenium_test/tests/Login_Test.py
@@ -1,19 +1,13 @@
-import random
-import string
 import nose
 from nose.tools import assert_equal, assert_true
 import sys
-import logging
-from selenium import webdriver
-import selenium_test.pages
-from selenium_test import pages
+sys.path.append('../../')
 from selenium_test.pages.onTap_login import onTapLogin
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from time import sleep
+from selenium_test.base.base_test import base_test
 import re
 
 
-class test_Login(object):
+class test_Login(base_test):
     _multiprocess_can_split_ = True
 
     envs = ['ie', 'firefox', 'chrome']
@@ -21,52 +15,31 @@ class test_Login(object):
     @classmethod
     def setUpClass(self):
         pass
-    
+
     def tearDown(self):
         self.driver.quit()
 
-    def getDriver(self, browser):
-        if browser == "firefox":
-            cap = DesiredCapabilities.FIREFOX
-        if browser == "ie":
-            cap = DesiredCapabilities.FIREFOX
-        if browser == "chrome":
-            cap = DesiredCapabilities.CHROME
-        if browser == "mobile chrome":
-            cap = {}
-            cap['browserName'] = "Chrome"
-            cap['platformName'] = "Android"
-            cap['deviceName'] = ""
-
-        return webdriver.Firefox()
-        #return webdriver.Remote(
-                             #   command_executor='http://10.238.242.50:4444/wd/hub',
-                              #  desired_capabilities=cap
-                               #)
-
     def test_user_can_login(self):
-
         for platform in self.envs:
             yield self.user_can_login, platform
 
     def user_can_login(self, browser):
-
         self.driver = self.getDriver("chrome")
         test_setup = {
                      'driver': self.driver,
                      'timeout': 30,
-                     'goto' : "http://ontapstaging.herokuapp.com/"
-                     }
+                     'goto': "http://ontapstaging.herokuapp.com/"
+        }
 
         Login = onTapLogin(test_setup)
-        Login.login("Matt.Watson", "1234")
+        Login.login("John.Smith", "1234")
         assert_true(re.match(".*/calendar", self.driver.current_url), "page did not load")
 
     def test_user_can_not_login(self):
         test_data = [
-            {'username': 'matt', 'password': '1234'},
+            {'username': 'John', 'password': '1234'},
             {'username': '', 'password': '1234'},
-            {'username': 'matt.watson', 'password': ''}
+            {'username': 'John.Smith', 'password': ''}
         ]
         for data in test_data:
             for platform in self.envs:
