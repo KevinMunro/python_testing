73b90b017806e6e33d5c71f5d4bab9bc2fa87faf
KevinMunro <Kevin.Munro@orasi.com>
2014-10-21 14:47:55 -0700

    Delete events

--- selenium_test/pages/onTap_calendar.py
+++ selenium_test/pages/onTap_calendar.py
@@ -8,7 +8,7 @@ class onTapCalendar(onTap):
 
     def find_event_by_title(self, title, action='exist'):
         title = self.driver.find_elements_by_xpath('//div[contains(@class, "title") and contains(text(), "' + title + '")]')
-        if len(title) > 1:
+        if len(title) >= 1:
             return True
         elif action == 'details':
             self.driver.find_element_by_xpath("(//div[contains(@class, 'title') and contains(text(), '" + title + "')]/../following-sibling::div/a[contains(text(), 'Details')])[1]")
@@ -20,6 +20,17 @@ class onTapCalendar(onTap):
             self.driver.find_element_by_xpath("(//div[contains(@class, 'title') and contains(text(), '" + title + "')]/../following-sibling::div/a[contains(text(), 'Delete')])[1]")
         elif action == 'request':
             self.driver.find_element_by_xpath("(//div[contains(@class, 'title') and contains(text(), '" + title + "')]/../following-sibling::div/a[contains(text(), 'Request To Attend')])[1]")
+
+
+    def get_event_by_title(self, title):
+        return self.driver.find_element_by_xpath('//div[contains(@class, "title") and contains(text(), "' + title + '")]')
+    
+    def attend_event(self, event):
+        event.find_child_with_text('attend').click()
+
+    def delete_event(self, deleteButton):
+        deleteButton.click()
+        deleteButton.find_element_by_xpath("/../../following-sibling::div/span/a[contains(text(),'Delete')]").click()
 
 
 
---
+++ selenium_test/tests/Delete_Event_Test.py
@@ -0,0 +1,62 @@
+import nose
+from nose.tools import assert_equal, assert_true, assert_regexp_matches
+from selenium_test.base import *
+from selenium_test.pages import *
+from collections import OrderedDict
+import time
+from selenium.webdriver.common.keys import Keys
+
+
+class test_Add_Event(base_test.test):
+
+    _multiprocess_can_split_ = True
+    run_locally = True
+
+    envs = ['chrome', 'ie', 'firefox']
+
+    def test_admin_can_add_event(self):
+        # Must put data in tuples to guarantee dictionary order
+        data = [OrderedDict((
+            ('event_style', "Lunch And Learn"),
+            ('event_title', "Lunch to detect"),
+            ('event_description', "Some Description that is longer than the title, but not war and peace long"),
+            ('add_host_button', 'click'),
+            ('add_host_select', 'Matt Watson'),
+            ('has_go_to_meeting', 'click'),
+            ('meeting_url', 'http://www.google.com'),
+            ('meeting_phone_number', '(336)259-7240'),
+            ('meeting_access_code', '123-123-123'),
+            ('set_event_date', '12/05/2014'),
+            ('event_start_time', '12:00pm'),
+            ('event_end_time', '1:00pm'),
+            ('event_restricted', 'click')
+        ))]
+        for test_data in data:
+            for platform in self.envs:
+                yield self.admin_can_delete_event, platform, test_data
+
+    def admin_can_delete_event(self, platform, data):
+        self.driver = self.getDriver(platform, self.run_locally)
+        test_setup = {
+            'driver': self.driver,
+            'timeout': 5
+        }
+
+        Login = onTap_login.onTapLogin(test_setup)
+        Login.goto("http://localhost:3000/")
+        Login.login("Company.Admin", "1234")
+
+        Login.add_new_event()
+
+        NewEvent = onTap_new_event.onTapNewEvent(test_setup)
+        NewEvent.fill_form(data)
+        NewEvent.submit_button().submit()
+
+        Calendar = onTap_calendar.onTapCalendar(test_setup)
+        assert_true(Calendar.is_success_message(), "No Success Message Displayed")
+        assert_regexp_matches(Calendar.success_message(), '.*Event "' + data['event_title'] + '" was created.*',
+                              'Success Message did not display correct event')
+
+        assert_true(Calendar.find_event_by_title(data['event_title']), "does not exist")
+        Calendar.delete_event(Calendar.get_event_by_title(data['event_title']))
+
